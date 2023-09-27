1838e738b645140d88840dba4b93410044df0f8f
Joel Jonsson Rapp <joel.jonsson.rapp@iths.se>
2023-09-27 11:13:36 +0200

    add navbar placeholder text

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-
+        <h3>{{ message }}</h3>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                message: 'Navbar placeholder'
+            }
+        }
     }
 </script>
 
