bce7ca87a88d45d7d77ba551d148753249812d42
hannah <hannah.degerheim@iths.se>
2023-09-28 12:41:34 +0200

    added fetch for categories in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h3>{{ message }}</h3>
-
+<ul>
+    <li v-for="item in apiData" :key="item.id">{{ item.name }}</li>
+</ul>
     </div>
 </template>
 
@@ -10,8 +12,15 @@ export default {
     data() {
         return {
             message: 'Uppstart av navbar',
+            apiData: [],
 
         }
+    },
+    created(){
+        fetch('https://jau22-recept-grupp2-yiqamvjp984a.reky.se/categories')
+        .then(response=>response.json())
+        .then(data=>{this.apiData=data;})
+        .catch(error=>{console.error('An error occured: ', error)})
     }
 }
 </script>
