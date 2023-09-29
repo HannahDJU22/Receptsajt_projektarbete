efc1e32aea81fef5b85caf195c1afbbb0352f01b
Joel Jonsson Rapp <joel.jonsson.rapp@iths.se>
2023-09-29 12:33:28 +0200

    Add category component and integrated into navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,26 +2,28 @@
     <div>
         <h3>{{ message }}</h3>
         <ul>
-            <li v-for="item in apiData" :key="item.id">{{ item.name }}</li>
+            <li v-for="item in apiData" :key="item.id"><Category :name="item.name"/></li>
         </ul>
     </div>
 </template>
 
 <script>
+import Category from './Category.vue';
 
 export default {
     data() {
         return {
             message: 'Uppstart av navbar',
             apiData: [],
-        }
+        };
     },
     created() {
         fetch('https://jau22-recept-grupp2-yiqamvjp984a.reky.se/categories')
             .then(response => response.json())
             .then(data => { this.apiData = data; })
-            .catch(error => { console.error('An error occured: ', error) })
-    }
+            .catch(error => { console.error('An error occured: ', error); });
+    },
+    components: { Category }
 }
 </script>
 
