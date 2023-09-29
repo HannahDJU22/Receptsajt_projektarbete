2dbdf1826f3662505017154b67049072eae8570f
hannah <hannah.degerheim@iths.se>
2023-09-29 13:32:36 +0200

    added Recipe comp and included some components

---
+++ src/components/Recipe.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+<Recipe_desc/>
+<Ingredients/>
+<Instructions/>
+    </div>
+</template>
+
+<script>
+import Ingredients from './Ingredients.vue';
+import Recipe_desc from './Recipe_desc.vue';
+import Ingredients from './Ingredients.vue';
+import Instructions from './Instructions.vue';
+
+export default {
+    components: { Recipe_desc, Ingredients, Instructions }
+}
+</script>
+
+<style scoped></style>
