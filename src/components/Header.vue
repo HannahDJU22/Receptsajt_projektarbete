9ea73ae5bbc3781a7515508aabe90225be36db91
hannah <hannah.degerheim@iths.se>
2023-10-03 10:13:43 +0200

    added content to header and welcome comps.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>
-
-    </div>
+    <header>
+<h1>{{ welcome }}</h1>
+    </header>
 </template>
 
 <script>
     export default {
-        
+        data(){
+            return{
+                welcome: 'Lord of the Sprint - Veganska recept by David'
+            }
+        }
     }
 </script>
 
--- src/components/WelcomeText.vue
+++ src/components/WelcomeText.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-
+{{ text }}
     </div>
 </template>
 
 <script>
     export default {
-        
+        data(){
+            return{
+            text: "Här kan du ta del av Davids magiska och veganska recept! ENJOY!"
+        }
+    }
     }
 </script>
 
