822818a18740844f6bcd6e21fa83f41f3d22fd0e
David Grobgeld <david.grobgeld@iths.se>
2023-10-09 14:00:40 +0200

    CommentSection w subcomps created

---
+++ src/components/CommentSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Kommentarer:</h2>
+        <CommentForm />
+        <CommentList />
+    </div>
+</template>
+
+<script>
+import CommentForm from './CommentForm.vue';
+import CommentList from './CommentList.vue';
+export default {
+    components: {
+        CommentForm,
+        CommentList
+    }
+}
+</script>
+
+<style scoped></style>
