6e34bc4e0efb93b24865e8c9c531dab46641d69b
hannah <hannah.degerheim@iths.se>
2023-10-12 11:09:41 +0200

    added validation and alert msg

--- src/components/CommentForm.vue
+++ src/components/CommentForm.vue
@@ -2,12 +2,13 @@
     <div>
         <fieldset>
             <legend>Lämna gärna en kommentar</legend>
-        <label for="name">Namn:</label><br>
-        <input type="text" id="name" placeholder="Ditt namn" name="name" v-model="userName"><br>
-        <label for="name">Kommentar:</label><br>
-        <input type="text" id="comment" placeholder="Skriv din kommentar" name="comment" v-model="commentBody">
-        <button type="button" @click="submitComment()">Skicka kommentar</button>
-    </fieldset>
+            <label for="name">Namn:</label><br>
+            <input type="text" id="name" placeholder="Ditt namn" name="name" v-model="userName" required><br>
+            <label for="name">Kommentar:</label><br>
+            <input type="text" id="comment" placeholder="Skriv din kommentar" name="comment" v-model="commentBody" required>
+            <button type="button" @click="submitComment()">Skicka kommentar</button>
+            <p v-if="isError" class="error-msg">Både namn och kommentar måste fyllas i!</p>
+        </fieldset>
     </div>
 </template>
 
@@ -17,7 +18,8 @@ export default {
     data() {
         return {
             userName: null,
-            commentBody: null
+            commentBody: null,
+            isError: false,
         };
     },
     props: {
@@ -25,14 +27,19 @@ export default {
     },
     methods: {
         submitComment() {
+            if (!this.userName || !this.commentBody) {
+                this.isError = true;
+                return;
+            }
+
             fetch(`https://jau22-recept-grupp2-yiqamvjp984a.reky.se/recipes/${this.recipeId}/comments`, {
                 method: 'POST',
                 headers: {
                     'Content-type': 'application/json; charset=UTF-8',
                 },
                 body: JSON.stringify({
-                  name:this.userName,
-                  comment:this.commentBody
+                    name: this.userName,
+                    comment: this.commentBody
 
                 })
             })
@@ -43,4 +50,9 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.error-msg{
+    color: red;
+    font-size: 20px;
+}
+</style>
