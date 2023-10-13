eead942d9023159ff74763ac06edad36e4063df3
hannah <hannah.degerheim@iths.se>
2023-10-13 09:50:49 +0200

    formatting and sorting date

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -2,13 +2,16 @@
     <div>
         <ul>
             <li v-for="comment in comments" :key="comment._id">
-                <div class="comment-list-date">{{ formatDate(comment.createdAt) }}</div>
-                <div class="comment-list-name"> {{ comment.name }}</div>
+                <div class="comment-list-box">
+                    <div class="comment-list-name"> {{ comment.name }}</div>
+                    <div class="comment-list-date">{{ formatDate(comment.createdAt) }}</div>
+                </div>
                 <div class="comment-list-comment">{{ comment.comment }}</div>
             </li>
         </ul>
     </div>
-</template>
+    </template>
+
 
 <script>
 export default {
@@ -33,7 +36,9 @@ export default {
         fetch(`https://jau22-recept-grupp2-yiqamvjp984a.reky.se/recipes/${this.recipeId}/comments`)
             .then(response => response.json())
             .then(data => {
-                this.comments = data
+                this.comments = data.slice().sort((a, b)=>{
+                    return new Date(b.createdAt) - new Date(a.createdAt);
+                })
             })
     }
 }
@@ -55,6 +60,11 @@ li {
     background-color: rgb(230, 230, 211);
 }
 
+.comment-list-box {
+    display: flex;
+    justify-content: space-between;
+}
+
 .comment-list-name {
     font-weight: bold;
     font-size: small;
@@ -63,4 +73,8 @@ li {
 .comment-list-comment {
     font-size: smaller;
 }
-</style>
+
+.comment-list-date {
+    text-align: right;
+    font-size: small;
+}</style>
