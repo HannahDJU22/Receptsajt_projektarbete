4e9dd5f7a37ef127dd1fbca53dc926c6258e9acc
David Grobgeld <david.grobgeld@iths.se>
2023-10-16 11:21:39 +0200

    rating correctly passed to component

--- src/components/UserRating.vue
+++ src/components/UserRating.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
         <p>Genomsnittligt omdöme:</p>
-        <font-awesome-icon v-for="index in userRating" :key="index" :icon="['fas', 'star']" />
+        <font-awesome-icon v-if="userRating" v-for="index in userRating" :key="index" :icon="['fas', 'star']" />
     </div>
 </template>
 
 <script>
-    export default {
-        props:{
-            userRating: Number,
-        }
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+export default {
+    components:{
+        FontAwesomeIcon
+    },
+    props: {
+        userRating: Number,
     }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
