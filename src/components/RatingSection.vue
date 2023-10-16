7f51af08f0233f47ac0d4957cee4e8df22f4925c
David Grobgeld <david.grobgeld@iths.se>
2023-10-16 13:28:34 +0200

    thank you message when rating submitted

--- src/components/RatingSection.vue
+++ src/components/RatingSection.vue
@@ -1,13 +1,19 @@
 <template>
+    <div>
         <h2>Vad tyckte du om receptet? Klicka på en stjärna för att ge ditt omdöme!</h2>
-        <font-awesome-icon v-for="starItem in starsTotal" @click="submitRating(starItem)" :icon="['fas', 'star']" />
+
+        <font-awesome-icon v-if="!ratingSubmitted" v-for="starItem in starsTotal" @click="submitRating(starItem)"
+            :icon="['fas', 'star']" />
+        <p v-else>Tack för ditt betyg!</p>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            starsTotal: 5
+            starsTotal: 5,
+            ratingSubmitted: false
         };
     },
     props: {
@@ -18,8 +24,8 @@ export default {
 
             alert(starNumber);
             let starsToPostObj = {
-                    rating: starNumber
-                };
+                rating: starNumber
+            };
 
             fetch(`https://jau22-recept-grupp2-yiqamvjp984a.reky.se/recipes/${this.recipeId}/ratings`, {
                 method: 'POST',
@@ -28,15 +34,16 @@ export default {
                 },
                 body: JSON.stringify(starsToPostObj)
             })
-            .then((response) => response.json())
-            .then((json) => {
-                    
+                .then((response) => response.text())
+                .then((json) => {
+
                     console.log(json)
-                                     
-            });
+
+                });
+            this.ratingSubmitted = true;
         }
     }
-    
+
 }
 </script>
 
